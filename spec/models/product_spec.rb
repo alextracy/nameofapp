c738f8c01f52a6ca1d4eb28c3af3c5a23eeb481f
alextracy <atracy60@gmail.com>
2015-11-13 14:17:30 -0600

    first unit test

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe Product do
+
+	context "when the product has comments" do
+
+		before {
+			@product = Product.create(:name => "race bike")
+			@user = User.create(:email => "air@plane.com", :password => "something")
+			@product.comments.create(:rating => 1, :user => @user, :body => "Aweful bike!")
+			@product.comments.create(:rating => 3, :user => @user, :body => "Ok bike")
+			@product.comments.create(:rating => 5, :user => @user, :body => "Awesome bike!") 
+		}
+		end
+
+		it "returns the average rating of all comments" do
+			expect(Product.comments). to eq "3"
+		end
+	end
+
+end
