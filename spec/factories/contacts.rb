b14fc37b34f86f6982c68ea622774b7670e69c52
alextracy <atracy60@gmail.com>
2015-11-16 17:55:53 -0600

    figuring out factorygirl

---
+++ spec/factories/contacts.rb
@@ -0,0 +1,9 @@
+# spec/factories/contacts.rb
+require 'rails_helper'
+
+FactoryGirl.define do
+  factory :contact do |f|
+    f.firstname { Faker::Name.first_name }
+    f.lastname { Faker::Name.last_name }
+  end
+end
