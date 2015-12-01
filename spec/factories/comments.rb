d3a28bd47fd0c47160d8ed40e4582ed2e48595f4
alextracy <atracy60@gmail.com>
2015-12-01 15:39:23 -0600

    trying to rake successfully

---
+++ spec/factories/comments.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+
+  factory :comment do 
+    body 'yolo'
+    user 'sdfom'
+    product 'a@b.com'
+    rating 3
+  end
+
+end
---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+	
+  factory :user do 
+    name 'sdfom'
+    email 'a@b.com'
+    password 'something'
+  end
+
+end
