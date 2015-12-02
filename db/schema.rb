1e2fc766c5ed8986e1ecbd57e0fd6f052545f1f7
alextracy <atracy60@gmail.com>
2015-12-02 13:40:43 -0600

    fixed factorygirl and added column(price) to products

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151110031827) do
+ActiveRecord::Schema.define(version: 20151202020825) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20151110031827) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "color"
+    t.decimal  "price"
   end
 
   create_table "users", force: :cascade do |t|
