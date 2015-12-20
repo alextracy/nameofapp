c46b8e9c289825a5b9ba2aacabdb91abbdfb9427
alextracy <atracy60@gmail.com>
2015-12-20 16:58:44 -0600

    adjusting jquery

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,4 +1,4 @@
-$(document).on('page:load ready', function(){
+$(document).on('ready page:load', function(){
 	$('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
 	$('.rated').raty({ path: '/assets', 
 		readOnly: true, 
