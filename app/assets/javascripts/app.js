d59269b44e6443577922c1f5e658ee5cae721053
alextracy <atracy60@gmail.com>
2016-01-08 16:32:50 -0600

    ng-repeat

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -5,5 +5,9 @@ $(document).on('ready page:load', function(){
 });
 
 app.controller('OrdersCtrl', ['$scope', function($scope){
-	$scope.name = "Bob";
+	$scope.orders =[
+	{id: 1, total: 24, product_id: 1, user_id: 1},
+	{id: 2, total: 7, product_id: 1},
+	{id: 3, total: 42, product_id: 1, user_id: 1}
+	];
 }]);
