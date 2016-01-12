70cafce2c7d8382d458b536b93ab4d40ce3ca168
alextracy <atracy60@gmail.com>
2016-01-12 02:29:45 -0600

    first draft of learning angularjs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,7 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require angular
+//= require angular-resource
 //= require turbolinks
 //= require jquery.elevateZoom-3.0.8.min
 //= require_tree .
