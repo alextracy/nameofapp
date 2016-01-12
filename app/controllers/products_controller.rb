96552c218464885710abe306aa471b581458aedf
alextracy <atracy60@gmail.com>
2016-01-12 15:30:23 -0600

    about done with angular

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
-
+  respond_to :json, :html
   before_action :set_product, only: [:show, :edit, :update, :destroy]
- 
+  
   # GET /products
   # GET /products.json
   def index
@@ -11,6 +11,7 @@ class ProductsController < ApplicationController
     else
       @products = Product.all
     end
+    respond_with @products
   end
 
   # GET /products/1
