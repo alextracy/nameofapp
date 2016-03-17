8bb3e75d9a0badd8d6e56e6e33b92783b8ac7a0d
alextracy <atracy60@gmail.com>
2016-03-16 21:21:59 -0600

    more styling

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -12,9 +12,9 @@ $(document).on('ready page:load', function(){
 
       $(window).scroll(function() { // check if scroll event happened
         if ($(document).scrollTop() > 12) { // check if user scrolled more than 50 from top of the browser window
-          $(".navbar-fixed-top").fadeOut(); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
+          $("nav").fadeOut(); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
         } else {
-          $(".navbar-fixed-top").css("background-color", "transparent").fadeIn(); // if not, change it back to transparent
+          $("nav").css("background-color", "transparent").fadeIn(); // if not, change it back to transparent
         }
       });
 
