9818aad7a4805d87bef3af3cd090c01983d7d6db
Alex Tracy <atracy60@gmail.com>
2016-04-14 09:32:54 -0600

    fixing orders routes for heroku use

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,8 @@ Rails.application.routes.draw do
 
   get 'products/index'
 
+  get 'orders/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
